9bbd0971a20333c0a5a5f16162386dc0dbb39a85
Fedor Korchazhnikov <butnotstupid@gmail.com>
2017-04-05 22:21:23 +0300

    Slack reporting token fix

--- src/main/java/net/bcnnm/notifications/slack/SlackChannelWriter.java
+++ src/main/java/net/bcnnm/notifications/slack/SlackChannelWriter.java
@@ -4,15 +4,15 @@ import allbegray.slack.SlackClientFactory;
 import allbegray.slack.webapi.SlackWebApiClient;
 
 public class SlackChannelWriter {
-    private final String USERNAME = "model-notifier";
-    private final String TOKEN = "xoxb-29794513911-Gbu0b8usJxQmM0Di56xkl2It";
+    private final String USERNAME = "test-notifier";
 
     private SlackWebApiClient slackClient;
     private final String channel;
 
     public SlackChannelWriter(String channel) {
         this.channel = channel;
-        slackClient = SlackClientFactory.createWebApiClient(TOKEN);
+        String token = System.getProperty("token");
+        slackClient = SlackClientFactory.createWebApiClient(token);
         slackClient.auth();
     }
 
